e33cf16606611b0401265110d69707ecda660b2b
bholloway546 <bholloway546@gmail.com>
2014-11-25 05:30:55 +0000

    Second

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,5 +1,5 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-  return "Hello" + firstName,+ "\nmessage"
+  return "Hello" + firstName,+ 
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
