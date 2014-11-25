63f4e9191d033afd0045b6c2280e715c42c88c42
bholloway546 <bholloway546@gmail.com>
2014-11-25 05:18:15 +0000

    Second

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,5 +1,5 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-return("Hello" + "\n\nfirstName" + "\n\nmessage" + "Sincerely," + "\nsenderName")
+return("Hello + \n\nfirstName + \n\nmessage + Sincerely, + \nsenderName")
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
