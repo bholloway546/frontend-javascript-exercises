645bc7131f1fef0ed5036c0c7c8d57d38c10449d
bholloway546 <bholloway546@gmail.com>
2014-11-30 04:29:09 +0000

    If-3

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -9,6 +9,21 @@ else if(guess<fav){
    return("You got it!"); 
   }
 }
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(num1,num2,num3,num4){
+  if(num1 === 3 || num1 === 5 || num1 === 7){
+} else if(num2===2){
+  
+}
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item,money){
+  if(item="MacBook Air" && money>999){
+  return("True");
+} else if(item="MacBook Pro" && money>1299){
+  return("True");
+} else if(item="Mac Pro" && money>2499){
+  return("True")
+} else if(item="Apple Sticker" && money>1){
+  return("True")
+} else(){
+  return("False")
+}
