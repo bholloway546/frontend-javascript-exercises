8c66fa4891556320119236756caa64839c3e06c7
bholloway546 <bholloway546@gmail.com>
2014-11-30 04:06:30 +0000

    If-1

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,14 @@
-module.exports.favoriteNumber = undefined;
-
+module.exports.favoriteNumber = function(fav,guess){
+  if(guess>fav){
+  return("Too High");
+}
+else if(guess<fav){
+  return("Too Low");
+}
+  else(guess=fav);{
+   return("You Got It!"); 
+  }
+}
 module.exports.checkLock = undefined;
 
 module.exports.canIGet = undefined;
