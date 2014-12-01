738f975c6d1952123154827d8d91524030bb8cda
bholloway546 <bholloway546@gmail.com>
2014-12-01 04:33:58 +0000

    Booleans-12

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -15,14 +15,19 @@ if(one===two){
 
 module.exports.inBetween = function(lower, middle, upper) {
 if(middle>lower && middle<upper){
-  return 5>4
+  return true
 } else{
   return 5<4
 }
 };
 
 module.exports.outsideRanges = function(number) {
-};
+  if((number<10 || number>20) && (number<=42 || number>75) && (number<=1 || number>=6)){
+     return true;
+} else{
+     return false;
+}
+}
 
 module.exports.nameAndPrice = function(name, price) {
 if((name=="NYTimes" || name == "LATimes") && (price>=1)){
