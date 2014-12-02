63c575c9dd566a8c6da2895b19b1d1005e3b4566
bholloway546 <bholloway546@gmail.com>
2014-12-02 01:34:31 +0000

    If-Final

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -10,22 +10,23 @@ else if(guess<fav){
   }
 }
 module.exports.checkLock = function(num1,num2,num3,num4){
-  if(num1 === 3 || num1 === 5 || num1 === 7){
-} else if(num2===2){
-  
+    if((num1 === 3 || num1 === 5 || num1 === 7) && (num2==2) && (num3>=5 && num3<=100) && (num4<9 || num4>100)){
+    return("correct");
+  } else{
+    return("incorrect");
+    }
 }
-
 module.exports.canIGet = function(item,money){
-  if (item="MacBook Air" && money>==999) {
-  return("True");
-} else if(item="MacBook Pro" && money>==1299){
-  return("True");
-} else if(item="Mac Pro" && money>==2499){
-  return("True");
-} else if(item="Apple Sticker" && money>==1){
-  return("True");
+  if (item=="MacBook Air" && money>=999){
+  return true;
+} else if(item=="MacBook Pro" && money>=1299){
+  return true;
+} else if(item=="Mac Pro" && money>=2499){
+  return true;
+} else if(item=="Apple Sticker" && money>=1){
+  return true;
 } else;
 {
-  return("False");
+  return false;
 }
 }
