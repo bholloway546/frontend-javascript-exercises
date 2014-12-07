0b4bf8c149ef3cf28dea02ada03d028c8f9ad54a
bholloway546 <bholloway546@gmail.com>
2014-12-07 22:29:58 +0000

    Array Definition Completed

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,13 @@
-module.exports.newArray = undefined;
+module.exports.newArray =function(a,b,c,d){
+  return array=[a,b,c,d];
+}
 
-module.exports.firstAndLast = undefined;
+
+module.exports.firstAndLast = function(array){
+  if(array.length>2){
+  var lastIndex=array.length-1;
+    var newArray=[array[0],array[lastIndex]]
+     return newArray;
+    } else
+      return console.log("Array needs to be bigger!");
+}
