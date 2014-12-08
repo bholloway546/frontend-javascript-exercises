328c1a638486a2481b83121ef09c16616ca588d5
bholloway546 <bholloway546@gmail.com>
2014-12-08 04:38:49 +0000

    Array Functions Completed

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,27 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(array){
+if (array.length>1){
+  var reverseArray=array.reverse();
+  reverseArray.unshift(1);
+  return reverseArray;
+} else{
+  return("This array needs more numbers!")
+}
+}
+module.exports.plusesEverywhere = function(array){
+if (array.length==0){
+  return "";
+}
+  if(array.length==1){
+    return array[0].toString();
+  }
+  var output=array[0].toString();
+  for(i=1;i<array.length;i++){
+    output+="+";
+    output+=array[i].toString();
+  }
+  return output
+}
 
-module.exports.plusesEverywhere = undefined;
-
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function(array){
+  return(array.length) + 1;
+}
