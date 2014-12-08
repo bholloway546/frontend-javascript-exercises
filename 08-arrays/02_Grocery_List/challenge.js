b9efc902bc1f33222dbbbe66341952ad4f847a8c
bholloway546 <bholloway546@gmail.com>
2014-12-08 04:34:22 +0000

    Grocery List Completed

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,15 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item,array){
+  for(i=0;i<array.length;i++){
+    if(array[i]==item){
+      return array;
+    }
+  }
+  array.push(item)
+  return array
+}
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(array){
+  array.sort();
+  array.reverse();
+  return array;
+}
