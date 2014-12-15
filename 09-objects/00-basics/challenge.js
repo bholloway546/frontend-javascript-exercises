5f8c2456baf23cef89c27818ca8eadeba800eef1
bholloway546 <bholloway546@gmail.com>
2014-12-15 06:21:06 +0000

    Objects Completed

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,28 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(string1,string2,array){
+  var bloc={title:string1,duration:string2,students:array};
+  return bloc;
+}
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object,newProp,newValue){
+ if (object.hasOwnProperty(newProp)){
+   return object;
+ }else{
+   object[newProp]= newValue;
+   return object;
+ }
+}
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter){
+  var letterString= "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+  return letterString;
+}
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item,money){
+  var priceList={
+    "MacBook Air":999,
+    "MacBook Pro":1299,
+    "Mac Pro":2499,
+    "Apple Sticker":1,
+  }
+  return (money>=priceList[item])
+}
